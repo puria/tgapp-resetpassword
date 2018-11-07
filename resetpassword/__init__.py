de9dc533d63c1d3ed60c3ebc7b996b3e03e0a4a7
Puria Nafisi Azizi <puria@dyne.org>
2018-11-07 20:13:20 +0100

    :arrow_up: TG2.4

--- resetpassword/__init__.py
+++ resetpassword/__init__.py
@@ -6,5 +6,10 @@ from tg.configuration import milestones
 
 
 def plugme(app_config, options):
-    app_config['_pluggable_resetpassword_config'] = options
+    try:
+        app_config['_pluggable_resetpassword_config'] = options
+    except TypeError:
+        app_config.update_blueprint({
+            '_pluggable_resetpassword_config': options
+        })
     return dict(appid='resetpassword', global_helpers=False)
